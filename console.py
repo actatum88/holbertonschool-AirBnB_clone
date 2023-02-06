162820964ab81ff8ef86cbc47e29f79c15c98314
Dustin Davis <davisdustin328@gmail.com>
2023-02-06 15:27:53 -0600

    Adding the ability to enter command prompt.

--- console.py
+++ console.py
@@ -1,3 +1,29 @@
-import cmd 
+#!usr/bin/python3
+"""Entry point for the command interpreter"""
+import cmd
+import sys
 
-class HBNBCommand(cmd.cmd)
+class HBNBCommand(cmd.Cmd):
+    
+    prompt = '(hbnb) '
+    
+    def emptyline(self):
+        """Will not execute anything on a emptyline"""
+        pass
+    
+    def can_exit(self):
+        """Helper for do_quit and do_EOF"""
+        return True
+    
+    def do_quit(self, arg):
+        """Quit the command interpreter."""
+        exit()
+
+    def do_EOF(self, arg):
+        """Quit the command interpreter on EOF."""        
+        exit()
+
+
+# code will not be executed when imported
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
