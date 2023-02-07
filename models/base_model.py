d06b6d30b2c9ae7d459b8d81a41e55a4950bba1e
actatum88 <alexandra.c.tatum@gmail.com>
2023-02-07 17:50:20 -0600

    Create BaseModel from dictionary.

--- models/base_model.py
+++ models/base_model.py
@@ -5,33 +5,37 @@ Base model for AirBnB Project
 
 from uuid import uuid4
 from datetime import datetime
+from .__init__ import storage
 
 
 class BaseModel:
     """Main Class Body"""
-
-    def __init__(self, name="Test"):
-        """ Initializer Function """
-        self.name = name
-        self.id = str(uuid4())
-        self.updated_at = self.created_at = datetime.now()
-
-    def __str__(self):
-        """
-        Prints string representation of the data object
-        """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        """Initializer function for Base Model"""
+        # if kwargs is not empty
+        # *args won’t be used
+        if kwargs:
+            # each key of this dictionary is an attribute name
+            # each value of this dictionary is the value of this attribute name
+            for key, value in kwargs.items():
+                # __class__ from kwargs is the only one that
+                # should not be added as an attribute
+                if key != '__class__':
+                    setattr(self, key, value)
+                else:
+                    self.id = str (uuid4())
+                    self.updated_at = self.created_at = datetime.now()
+                    storage.new(self)
+            
 
     def save(self):
         """
         Updates date of the object.
         """
         self.updated_at = datetime.now()
-
+        
     def to_dict(self):
-        """
-        Returns a dictionary with all object data.
-        """
+        """Returns a dictionary with all object data."""
         return dict(self.__dict__,
                     __class__=self.__class__.__name__,
                     updated_at=self.updated_at.isoformat(),
---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Class FileStorage: serializes instances to a JSON file
+    and deserializes JSON file to instances."""
+import json
+
+
+class FileStorage:
+    # string - path to the JSON file (ex: file.json)
+    __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        #Serialization is the process of converting the state of an object, that is, 
+        #the values of its properties, into a form that can be stored or transmitted.
+        with open(self.__file_path, 'w') as f:
+            f.write(json.dumps({key: value.to_dict() for key, value
+                                in self.__objects.items()}))
+
+    def reload(self):
+        """deserializes the JSON file to __objects
+            (only if the JSON file (__file_path) exists;
+            otherwise, do nothing. If the file doesn’t exist,
+            no exception should be raised)"""
+        #Deserialization reconstructs an object from the serialized form.
